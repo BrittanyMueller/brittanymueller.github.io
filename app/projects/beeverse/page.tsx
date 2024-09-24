a1c21559ac14e0086d42cf447ca6a8113c5fa4b1
Brittany Mueller <brittany_mueller@sympatico.ca>
2024-09-24 01:46:42 -0400

    Updated beeverse project page to use react unity webgl package.

--- app/projects/beeverse/page.tsx
+++ app/projects/beeverse/page.tsx
@@ -1,88 +1,64 @@
-'use client'
-import { useEffect, useRef } from 'react';
-import '/public/webgl_build/TemplateData/style.css';
+"use client"
+import { useEffect } from "react"
+import { Unity, useUnityContext } from "react-unity-webgl"
+import styles from './Beeverse.module.css'
 
 const BeeversePage = () => {
 
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const loadingRef = useRef<HTMLDivElement>(null);
-  const progressRef = useRef<HTMLDivElement>(null);
-  const fullscreenButtonRef = useRef<HTMLDivElement>(null);
-  
-  let unityInstance : any = null;
+  const buildUrl = "/webgl_build/Build"
+  const { unityProvider, loadingProgression, isLoaded, unload } = useUnityContext({
+    loaderUrl: `${buildUrl}/Beeverse.loader.js`,
+    dataUrl: `${buildUrl}/Beeverse.data`,
+    frameworkUrl: `${buildUrl}/Beeverse.framework.js`,
+    codeUrl: `${buildUrl}/Beeverse.wasm`,
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Brittany Mueller",
+    productName: "Beeverse",
+    productVersion: "1.0",
+  })
 
   useEffect(() => {
-    // Initialize loader script to run webgl build
-    var buildUrl = "/webgl_build/Build";
-    var config = {
-      dataUrl: buildUrl + "/Beeverse.data",
-      frameworkUrl: buildUrl + "/Beeverse.framework.js",
-      codeUrl: buildUrl + "/Beeverse.wasm",
-      streamingAssetsUrl: "StreamingAssets",
-      companyName: "Brittany Mueller",
-      productName: "Beeverse",
-      productVersion: "1.0",
-    };
-
-    const canvas = canvasRef.current;
-    const loadingBar = loadingRef.current;
-    const progressBar = progressRef.current;
-    const fullscreenBtn = fullscreenButtonRef.current;
-
-    if (!canvas || !loadingBar || !progressBar || !fullscreenBtn) {
-      console.error('Failed to find required DOM elements.');
-      return;
-    }
-
-    var loaderScript = document.createElement("script");
-    loaderScript.src = buildUrl + "/Beeverse.loader.js";
-    loaderScript.onload = () => {
-      // @ts-ignore, function within loader script
-      createUnityInstance(canvas, config, (progress: number) => {
-        // Changes width of progress bar to show % complete
-        progressBar.style.width = 100 * progress + "%";
-      }).then((instance : any) => {
-        unityInstance = instance;
-        loadingBar.style.display = "none";
-        fullscreenBtn.onclick = () => {
-          unityInstance.SetFullscreen(1);
-        };
-      }).catch((message: string) => {
-        alert(message);
-      });
-    };
-    document.body.appendChild(loaderScript);
-
     return () => {
-      // Remove loader script when component unmounts 
-      document.body.removeChild(loaderScript);
-      if (unityInstance) {
-        unityInstance.Quit();
+      // Clean up component during unmount, if fully loaded
+      if (isLoaded) {
+        unload().catch((error) => {
+          console.error("Failed to unload Unity instance:", error)
+        })
       }
-    };
-  }, []); // Empty dependency so only runs once 
-
+    }
+  }, [isLoaded])  // Loaded changes to false when navigating away
 
   return (
-    <div>
-      <h2>Beeverse Unity WebGL Build</h2>
-      <div id="unity-container">
-        <canvas id="unity-canvas" ref={canvasRef} tabIndex={-1}></canvas>
-        <div id="unity-loading-bar" ref={loadingRef}>
-          <div id="unity-logo"></div>
-          <div id="unity-progress-bar-empty">
-            <div id="unity-progress-bar-full" ref={progressRef}></div>
+    <div className={styles.unityContainer}>
+      { !isLoaded && (
+        <div className={styles.loadingContainer}>
+          <p>Loading... {Math.round(loadingProgression * 100)}%</p>
+          <div className={styles.loadingBar}>
+            <div
+              className={styles.progress}
+              style={{ width: `${loadingProgression * 100}%` }}
+            />
           </div>
         </div>
-        <div id="unity-warning"> </div>
-        <div id="unity-footer">
-          <div id="unity-webgl-logo"></div>
-          <div id="unity-fullscreen-button" ref={fullscreenButtonRef}></div>
-          <div id="unity-build-title">Beeverse</div>
-        </div>
+      )}
+      <div className={styles.canvasWrapper}>
+        <Unity 
+          unityProvider={unityProvider} 
+          className={styles.unityCanvas}
+          devicePixelRatio={1.5}  // Looks awful & blurry with default 1
+          tabIndex={-1}
+        />
       </div>
+      {/* TODO need another outer container for more content, loading bar relative to unityContainer */}
+      <p> 
+        Beeverse is a bee colony survival city builder. Your objective is to collect and generate enough resources to keep the hive alive!
+        Beeverse started as a final project in my game programming course but turned into a passion project of mine. 
+        Check out the GitLab repository to learn the&nbsp;
+        <a className={styles.link} href="https://gitlab.larrycloud.ca/cis4820/Beeverse#controls" target="_blank">controls.</a> 
+      </p>
     </div>
   )
+
 }
 
-export default BeeversePage;
+export default BeeversePage
