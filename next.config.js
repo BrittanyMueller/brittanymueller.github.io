b73379d0726a8d0470212665e7b872a267a1e6a5
Brittany Mueller <brittany_mueller@sympatico.ca>
2024-10-04 13:51:16 -0400

    remove basepath

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/website",
-  output: "export",  // enables static exports
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
